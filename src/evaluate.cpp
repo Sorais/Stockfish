f580e0a6d520eebd5cb5a092f98941f55c212e6f
LerhFengLow <53205850+LerhFengLow@users.noreply.github.com>
2022-10-26 17:42:23 +0800

    Update evaluate.cpp
    See if we can keep the definition of "complexity" closer to what the word itself means.

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1073,14 +1073,14 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  416 * nnueComplexity
                         + 424 * abs(psq - nnue)
-                        + (optimism  > 0 ? int(optimism) * int(psq - nnue) : 0)
                         ) / 1024;
 
       // Return hybrid NNUE complexity to caller
       if (complexity)
           *complexity = nnueComplexity;
 
-      optimism = optimism * (269 + nnueComplexity) / 256;
+      optimism = optimism * (269 + nnueComplexity
+          + (optimism > 0 ? (int(optimism) * int(psq - nnue)) / 1024 : 0)) / 256;
       v = (nnue * scale + optimism * (scale - 754)) / 1024;
   }
 
